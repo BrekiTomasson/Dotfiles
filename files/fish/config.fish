9ed4c8309e72943c034974510ad4bef407e2057d
Breki Tomasson <breki.tomasson@gmail.com>
2020-08-10 13:22:34 +0200

    Added additional configuration files

---
+++ files/fish/config.fish
@@ -0,0 +1,3 @@
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/Users/brekitomasson/Projects/Dotfiles/scripts" $fish_user_paths
+
---
+++ files/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/brekitomasson/.local/share/omf/themes/eden/fish_prompt.fish
