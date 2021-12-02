91927d1153cb1bc44e03a03ef314c5f1591548b0
Breki Tomasson <breki.tomasson@gmail.com>
2021-12-02 14:20:31 +0100

    update

--- files/fish/functions/__fish_describe_command.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /var/folders/nw/t_82d95n6_78k8c123p4rndh0000gn/T//fish.VgUcfo/__fish_describe_command.fish @ line 2
-function __fish_describe_command
-  # Commented out due to bad macOS behavior.
-end
