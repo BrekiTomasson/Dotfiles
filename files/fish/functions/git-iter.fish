37eb863ddfe2124e8ac05c60673301b4fc2a515e
Breki Tomasson <breki.tomasson@gmail.com>
2022-12-08 13:54:16 +0100

    :zap: minor iteration

---
+++ files/fish/functions/git-iter.fish
@@ -0,0 +1,3 @@
+function git-iter
+  git add . && git commit -m ":zap: minor iteration" && git push
+end
--- files/fish/functions/git-wip.fish
+++ files/fish/functions/git-wip.fish
@@ -1,3 +1,3 @@
-function wip
+function git-wip
   git add . && git commit -m ":construction: work in progress" && git push
 end
